e820861916264086f230f71cfdece6df58e333cc
naman <naman.jain@publicissapient.com>
2020-04-12 17:28:52 +0530

    added initial test setup

---
+++ components/App/__tests__/App.test.jsx
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import AppProvider from '../AppProvider';
+import AppComponent from '../App';
+
+describe('App Component', () => {
+  it('App component should render AppProvider', () => {
+    const posts = [];
+    const component = shallow(<AppComponent posts={posts} />);
+
+    expect(component.is(AppProvider)).toBeTruthy();
+  });
+});
